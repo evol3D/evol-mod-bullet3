6718636e8b215175ec0cb1bf671a6e3176b8f3a6
Robear Selwans <robear.selwans@outlook.com>
2021-05-17 22:55:57 +0200

    Updated Debugger to use active game camera
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

---
+++ src/cpp/visual-dbg/BulletDbg.cpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#include "BulletDbg.hpp"
+#include <evol/common/ev_log.h>
+
+#define NAMESPACE_MODULE evmod_glfw
+#include <evol/meta/namespace_import.h>
+
+#define TYPE_MODULE evmod_game
+#include <evol/meta/type_import.h>
+#define NAMESPACE_MODULE evmod_game
+#include <evol/meta/namespace_import.h>
+
+BulletDbg::BulletDbg()
+{
+    window_module = evol_loadmodule("window");
+    game_module = evol_loadmodule("game");
+    DEBUG_ASSERT(window_module);
+    DEBUG_ASSERT(game_module);
+    IMPORT_NAMESPACE(Window, window_module);
+    IMPORT_NAMESPACE(DbgWindow, window_module);
+    IMPORT_NAMESPACE(imGL, window_module);
+    IMPORT_NAMESPACE(Camera, game_module);
+
+    dbgWindow = DbgWindow->create(800, 600, "Physics Debug");
+
+    windowDestroyed = 0;
+}
+
+BulletDbg::~BulletDbg()
+{
+    DbgWindow->destroy(dbgWindow);
+    evol_unloadmodule(window_module);
+    evol_unloadmodule(game_module);
+}
+
+#define bt2evVec3(v) (Vec3){v.x(), v.y(), v.z()}
+
+void 
+BulletDbg::drawLine(
+    const btVector3& from, 
+    const btVector3& to, 
+    const btVector3& color)
+{
+    imGL->setColor3f(1.0, color.y(), color.z());
+    imGL->drawLine((Vec3)bt2evVec3(from), (Vec3)bt2evVec3(to));
+}
+
+void 
+BulletDbg::drawContactPoint(
+    const btVector3& PointOnB, 
+    const btVector3& normalOnB, 
+    btScalar distance, 
+    int lifeTime, 
+    const btVector3& color)
+{
+    UNIMPLEMENTED();
+}
+
+void 
+BulletDbg::reportErrorWarning(
+    const char* warningString)
+{
+    ev_log_error("Bullet Error: %s", warningString);
+}
+
+void 
+BulletDbg::draw3dText(
+    const btVector3& location, 
+    const char* textString)
+{
+    UNIMPLEMENTED();
+}
+
+void 
+BulletDbg::setDebugMode(
+    int debugMode)
+{
+    UNIMPLEMENTED();
+}
+
+int 
+BulletDbg::getDebugMode() const
+{
+    return DBG_DrawWireframe | DBG_DrawAabb;
+}
+
+void 
+BulletDbg::startFrame()
+{
+    U32 width, height;
+    Window->getSize(dbgWindow, &width, &height);
+    DbgWindow->startFrame(dbgWindow);
+
+    ObjectID activeCamera = Camera->getActive();
+
+    Matrix4x4 projectionMat;
+    Matrix4x4 viewMat;
+    Camera->getViewMat(activeCamera, viewMat);
+    Camera->getProjectionMat(activeCamera, projectionMat);
+
+    imGL->setViewport(0, 0, width, height);
+    imGL->setCameraViewMat(viewMat);
+    imGL->setCameraProjMat(projectionMat);
+
+    imGL->setClearColor((Vec4){0.0, 0.0, 0.0, 1.0});
+    imGL->clearBuffers();
+}
+
+void 
+BulletDbg::endFrame()
+{
+    DbgWindow->endFrame(dbgWindow);
+    windowDestroyed = DbgWindow->update(dbgWindow);
+}
--- src/cpp/visual-dbg/BulletDbg.hpp
+++ src/cpp/visual-dbg/BulletDbg.hpp
@@ -7,108 +7,27 @@
 #define TYPE_MODULE evmod_glfw
 #include <evol/meta/type_import.h>
 
-#define NAMESPACE_MODULE evmod_glfw
-#include <evol/meta/namespace_import.h>
-
 class BulletDbg: public btIDebugDraw
 {
 private:
     evolmodule_t window_module;
+    evolmodule_t game_module;
     WindowHandle dbgWindow;
 
 public:
     I32 windowDestroyed;
 
 public:
-    BulletDbg()
-    {
-        window_module = evol_loadmodule("window");
-        DEBUG_ASSERT(window_module);
-        IMPORT_NAMESPACE(Window, window_module);
-        IMPORT_NAMESPACE(DbgWindow, window_module);
-        IMPORT_NAMESPACE(imGL, window_module);
+    void startFrame();
+    void endFrame();
 
-        dbgWindow = DbgWindow->create(800, 600, "Physics Debug");
+    BulletDbg();
+    ~BulletDbg();
 
-        windowDestroyed = 0;
-    }
-
-    ~BulletDbg()
-    {
-        DbgWindow->destroy(dbgWindow);
-        evol_unloadmodule(window_module);
-    }
-
-	inline void 
-    drawLine(
-        const btVector3& from, 
-        const btVector3& to, 
-        const btVector3& color) override
-    {
-        imGL->setColor3f(1.0, color.y(), color.z());
-        imGL->drawLine(
-            from.x(), from.y(), from.z(),
-            to.x(), to.y(), to.z());
-    }
-
-	inline void 
-    drawContactPoint(
-        const btVector3& PointOnB, 
-        const btVector3& normalOnB, 
-        btScalar distance, 
-        int lifeTime, 
-        const btVector3& color) override
-    {
-        UNIMPLEMENTED();
-    }
-
-	inline void 
-    reportErrorWarning(
-        const char* warningString) override
-    {
-        ev_log_error("Bullet Error: %s", warningString);
-    }
-
-	inline void 
-    draw3dText(
-        const btVector3& location, 
-        const char* textString) override
-    {
-        UNIMPLEMENTED();
-    }
-
-	inline void 
-    setDebugMode(
-        int debugMode) override
-    {
-        UNIMPLEMENTED();
-    }
-
-	inline int 
-    getDebugMode() const override
-    {
-        return DBG_DrawWireframe | DBG_DrawAabb;
-    }
-
-    inline void 
-    startFrame()
-    {
-        U32 width, height;
-        Window->getSize(dbgWindow, &width, &height);
-        DbgWindow->startFrame(dbgWindow);
-
-        imGL->setViewport(0, 0, width, height);
-        imGL->projPersp(120, (F32)width/(F32)height, 0.1, 1000.0);
-        imGL->setCameraView(0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
-
-        imGL->setClearColor(0.0, 0.0, 0.0, 1.0);
-        imGL->clearBuffers();
-    }
-
-    inline void 
-    endFrame()
-    {
-        DbgWindow->endFrame(dbgWindow);
-        windowDestroyed = DbgWindow->update(dbgWindow);
-    }
+    void drawLine( const btVector3& from, const btVector3& to, const btVector3& color) override;
+    void drawContactPoint( const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color) override;
+    void reportErrorWarning( const char* warningString) override;
+    void draw3dText( const btVector3& location, const char* textString) override;
+    void setDebugMode( int debugMode) override;
+    int getDebugMode() const override;
 };
